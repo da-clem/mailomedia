f7b108dee632974009e51c31bab687feb6a51320
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2019-10-04 18:57:21 +0200

    mobile portfolio

--- src/components/projects/GaleriesLafayettes.jsx
+++ src/components/projects/GaleriesLafayettes.jsx
@@ -3,25 +3,25 @@ import React from 'react'
 export default function GaleriesLafayettes() {
   return (
     <div className="pb-8">
-      <div className="row py-3">
-        <div className="col-12 col-lg-6">
+      <div className="row py-3 text-center text-lg-left">
+        <div className="col-12 col-lg-6 ">
           <h1>Galeries Lafayettes</h1>
           <p className="text-primary small">#COS #SOLDES #BLACKFRIDAY #FRENCHDAYS</p>
         </div>
-        <div className="col-12 col-lg-6 d-flex justify-content-end">
-          <div className="pl-3 flex-grow-0">
+        <div className="col-12 col-lg-6 d-flex justify-content-around justify-content-lg-end mt-2 mt-lg-0">
+          <div className="pl-lg-3 flex-grow-0">
             <p className="small font-weight-bold text-uppercase text-primary">
               objectif
             </p>
             <p>CAC<br />COS</p>
           </div>
-          <div className="pl-3 flex-grow-0">
+          <div className="pl-lg-3 flex-grow-0">
             <p className="small font-weight-bold text-uppercase text-primary">
               secteur
             </p>
             <p>Prêt à porter<br />parfumerie<br />make-up</p>
           </div>
-          <div className="pl-3 flex-grow-0">
+          <div className="pl-lg-3 flex-grow-0">
             <p className="small font-weight-bold text-uppercase text-primary">
               timeframe
             </p>
@@ -30,7 +30,7 @@ export default function GaleriesLafayettes() {
         </div>
       </div>
       <div className="bg-light py-10 w-100"></div>
-      <div className="d-flex justify-content-center text-center py-3 px-10">
+      <div className="d-flex justify-content-center text-center py-3 px-lg-10">
         <p>
           Nous accompagnons les Galeries Lafayette
           sur leur problématique d’acquisition durant
@@ -40,19 +40,16 @@ export default function GaleriesLafayettes() {
         </p>
       </div>
       <div className="row">
-        <div className="col-12 pb-3">
-          <h3>Data-keys</h3>
-        </div>
-        <div className="col-12 col-lg-4 text-center">
-          <p className="display-2 text-primary">14.8</p>
+        <div className="col-12 col-lg-4 text-center mb-3">
+          <p className="display-2 text-primary mb-1">14.8</p>
           <p>Moyenne du COS (cost of sales) sur nos campagnes</p>
         </div>
-        <div className="col-12 col-lg-4 text-center">
-          <p className="display-2 text-primary">4 jours</p>
+        <div className="col-12 col-lg-4 text-center mb-3">
+          <p className="display-2 text-primary mb-1">4 jours</p>
           <p>Délais moyens de mise en place d’une <br /> opération d’acquisition</p>
         </div>
-        <div className="col-12 col-lg-4 text-center">
-          <p className="display-2 text-primary">
+        <div className="col-12 col-lg-4 text-center mb-3">
+          <p className="display-2 text-primary mb-1">
             7,833<small>M</small>
           </p>
           <p>Utilisateurs touchés par nos campaignes</p>
