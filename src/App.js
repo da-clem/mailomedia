4b17f4691cfdd17110c834e71e9b1d3a6d1b7fe3
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2019-10-29 12:19:20 +0100

    node v change

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App({ location }) {
         <Helmet>
           <meta charSet="utf-8" />
           <meta name="viewport" content="width=device-width"></meta>
-          <title>Mailo Media | Agence digitale performante</title>
+          <title>Mailo Media | Agence Webmarketing</title>
         </Helmet>
         <Switch location={location}>
           <Route path="/portfolio" component={lazy(() => import('./PortfolioPage'))} />
